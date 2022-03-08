30e55202a0de6041399142a36ef2fd91514d358b
michal.boltowicz <micbo26@gmail.com>
2022-03-08 20:42:09 +0100

    Dodaj pliki projektów.

---
+++ biometria 1/Algorithm.cs
@@ -0,0 +1,90 @@
+﻿using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+public static class Algorithm
+{
+    public static Bitmap Histogram(Bitmap bmp)
+    {
+        var data = bmp.LockBits(
+            new Rectangle(0, 0, bmp.Width, bmp.Height),
+            System.Drawing.Imaging.ImageLockMode.ReadWrite,
+            System.Drawing.Imaging.PixelFormat.Format24bppRgb
+        );
+        var bmpData = new byte[data.Stride * data.Height];
+
+        Marshal.Copy(data.Scan0, bmpData, 0, bmpData.Length);
+        // Przerzuci z Bitmapy do tablicy
+
+        int[] histogram = new int[256];
+        foreach (byte i in bmpData)
+            ++histogram[i];
+        double max = histogram.Max();
+        for (int i = 0; i < histogram.Length; i++)
+            histogram[i] = (int)(histogram[i] / max * 512.0);
+
+        bmpData = new byte[bmpData.Length];
+        for (int i = 0; i < bmpData.Length; i++)
+            bmpData[i] = 255;
+        for (int i = 0; i < histogram.Length; i++)
+        {
+            for (int j = 0; j < histogram[i]; j++)
+            {
+                int index = i * 3 + (511 - j) * data.Stride;
+
+                bmpData[index + 0] =
+                bmpData[index + 1] =
+                bmpData[index + 2] = 0;
+            }
+        }
+
+        Marshal.Copy(bmpData, 0, data.Scan0, bmpData.Length);
+        // Przerzuci z tablicy do Bitmapy
+
+        bmp.UnlockBits(data);
+        return bmp;
+    }
+
+    public static Bitmap BinaryThreshold(Bitmap bmp, byte threshold)
+    {
+        var data = bmp.LockBits(
+            new Rectangle(0, 0, bmp.Width, bmp.Height),
+            System.Drawing.Imaging.ImageLockMode.ReadWrite,
+            System.Drawing.Imaging.PixelFormat.Format24bppRgb
+        );
+
+        /*
+         * 3x4 => 12 pikseli
+         * 9x4 => 36 bajtów (RGB)
+         */
+
+        // Width - szerokość w pikselach
+        // Stride - szerokość w bajtach
+        // Stride => Width * 3
+        var bmpData = new byte[data.Stride * data.Height];
+
+        Marshal.Copy(data.Scan0, bmpData, 0, bmpData.Length);
+        // Przerzuci z Bitmapy do tablicy
+
+        for (int i = 0; i < bmpData.Length; i += 3)
+        {
+            byte r = bmpData[i + 0];
+            byte g = bmpData[i + 1];
+            byte b = bmpData[i + 2];
+
+            byte mean = (byte)((r + g + b) / 3);
+
+            bmpData[i + 0] =
+            bmpData[i + 1] =
+            bmpData[i + 2] = mean > threshold
+                ? byte.MaxValue
+                : byte.MinValue;
+        }
+
+        Marshal.Copy(bmpData, 0, data.Scan0, bmpData.Length);
+        // Przerzuci z tablicy do Bitmapy
+
+        bmp.UnlockBits(data);
+        return bmp;
+    }
+}
---
+++ biometria 1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace biometria_1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+
+    string? SourceLink = null;
+    Bitmap sourceImage = null;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OpenFile(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        if (openFileDialog.ShowDialog() == true)
+        {
+            Uri fileUri = new Uri(openFileDialog.FileName);
+            this.sourceImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), fileUri);
+            ReadyImage.Source = new BitmapImage(fileUri);
+        }
+}
