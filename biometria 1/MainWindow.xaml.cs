4b06403a70a37235e631888d669c85aba4fee876
michal.boltowicz <micbo26@gmail.com>
2022-03-08 20:48:30 +0100

    Open Image

--- biometria 1/MainWindow.xaml.cs
+++ biometria 1/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,6 +11,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -36,8 +38,22 @@ public partial class MainWindow : Window
         OpenFileDialog openFileDialog = new OpenFileDialog();
         if (openFileDialog.ShowDialog() == true)
         {
-            Uri fileUri = new Uri(openFileDialog.FileName);
-            this.sourceImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), fileUri);
-            ReadyImage.Source = new BitmapImage(fileUri);
+            string fileName = openFileDialog.FileName;
+            this.sourceImage = new Bitmap($"{fileName}");
+            ReadyImage.Source = ImageSourceFromBitmap(this.sourceImage);
         }
+    }
+
+    [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool DeleteObject([In] IntPtr hObject);
+    public ImageSource ImageSourceFromBitmap(Bitmap bmp)
+    {
+        var handle = bmp.GetHbitmap();
+        try
+        {
+            return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+        }
+        finally { DeleteObject(handle); }
+    }
 }
